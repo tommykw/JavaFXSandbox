2d99f90b318eda810a44d6a7ae319701fb6028a6
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-09-12 22:55:56 +0900

    add form

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,31 +4,53 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Button btn = new Button();
-        btn.setText("hello world");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("hello world");
-            }
-        });
+        primaryStage.setTitle("JavaFx Welcome");
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
 
-        Scene scene = new Scene(root, 300, 250);
-        primaryStage.setTitle("hello world");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text sceneTitle  = new Text("Welcome");
+        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(sceneTitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label password = new Label("Password");
+        grid.add(password, 0, 2);
+
+        PasswordField passwordBox = new PasswordField();
+        grid.add(passwordBox, 1, 2);
+
+        Scene scene = new Scene(grid, 300, 275);
         primaryStage.setScene(scene);
+
         primaryStage.show();
     }
 
