bde1acc733acc6aca86effb3b664a07afc5de274
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-09-16 12:55:56 +0900

    recompose

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,7 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -25,61 +26,10 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Scene scene = new Scene(root, 300, 300);
-        primaryStage.setTitle("FXML Welcome");
+        Group root = new Group();
+        Scene scene = new Scene(root, 800, 600, Color.BLACK);
         primaryStage.setScene(scene);
         primaryStage.show();
-//        primaryStage.setTitle("JavaFx Welcome");
-//
-//        GridPane grid = new GridPane();
-//        grid.setAlignment(Pos.CENTER);
-//        grid.setHgap(10);
-//        grid.setVgap(10);
-//        grid.setPadding(new Insets(25, 25, 25, 25));
-//
-//        Text sceneTitle  = new Text("Welcome");
-//        sceneTitle.setId("welcome-text");
-//        sceneTitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-//        grid.add(sceneTitle, 0, 0, 2, 1);
-//
-//        Label userName = new Label("User Name");
-//        grid.add(userName, 0, 1);
-//
-//        TextField userTextField = new TextField();
-//        grid.add(userTextField, 1, 1);
-//
-//        Label password = new Label("Password");
-//        grid.add(password, 0, 2);
-//
-//        PasswordField passwordBox = new PasswordField();
-//        grid.add(passwordBox, 1, 2);
-//
-//        Scene scene = new Scene(grid, 300, 275);
-//        primaryStage.setScene(scene);
-//        scene.getStylesheets().add(
-//                Login.class.getResource("Login.css").toExternalForm()
-//        );
-//        primaryStage.show();
-//
-//        final Text actionTarget = new Text();
-//        grid.add(actionTarget, 1, 6);
-//        actionTarget.setId("actiontarget");
-//
-//        Button btn = new Button("Sign in");
-//        btn.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent event) {
-//                actionTarget.setFill(Color.FIREBRICK);
-//                actionTarget.setText("Sign in button pressed");
-//            }
-//        });
-//        HBox hbox = new HBox(10);
-//        hbox.setAlignment(Pos.BOTTOM_RIGHT);
-//        hbox.getChildren().add(btn);
-//        grid.add(hbox, 1, 4);
-//
-//        primaryStage.show();
     }
 
     public static void main(String[] args) {
