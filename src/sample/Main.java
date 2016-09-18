3fecb2f670c88089f02664599305f8114d9a27d5
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-09-18 14:52:40 +0900

    add rainbow

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -17,7 +17,11 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -39,6 +43,21 @@ public class Main extends Application {
         }
         root.getChildren().add(circles);
         Scene scene = new Scene(root, 800, 600, Color.BLACK);
+        Rectangle colors = new Rectangle(
+                scene.getWidth(),
+                scene.getHeight(),
+                new LinearGradient(0f, 1f, 1f, 0f, true, CycleMethod.NO_CYCLE, new Stop[] {
+                        new Stop(0, Color.web("#f8bd55")),
+                        new Stop(0.14, Color.web("#c0fe56")),
+                        new Stop(0.28, Color.web("#5dfbc1")),
+                        new Stop(0.43, Color.web("#64c2f8")),
+                        new Stop(0.57, Color.web("#be4af7")),
+                        new Stop(0.71, Color.web("#ed5fc2")),
+                        new Stop(0.85, Color.web("#ef504c")),
+                        new Stop(1, Color.web("#f2660f")),}));
+        colors.widthProperty().bind(scene.widthProperty());
+        colors.heightProperty().bind(scene.heightProperty());
+        root.getChildren().add(colors);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
