d705dae7204947289cc32e4b9414aba5694742a6
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-09-19 00:34:02 +0900

    add bank

---
+++ src/bank/Main.java
@@ -0,0 +1,25 @@
+package bank;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by tommy on 2016/09/19.
+ */
+public class Main extends Application {
+    private static String ui = "ui.fxml";
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource(ui));
+        Scene frame = new Scene(root);
+        primaryStage.isResizable();
+        primaryStage.setTitle("bank");
+        primaryStage.setScene(frame);
+        primaryStage.centerOnScreen();
+        primaryStage.show();
+    }
+}
