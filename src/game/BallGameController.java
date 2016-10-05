e787fde7c17ebabf6ce66f5f9b8c951620f75bcd
kenji tomita <destinys.lotus.1108mn@gmail.com>
2016-10-05 23:57:48 +0900

    add BallGameController

---
+++ src/game/BallGameController.java
@@ -0,0 +1,16 @@
+package game;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by tommy on 2016/10/05.
+ */
+public class BallGameController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
